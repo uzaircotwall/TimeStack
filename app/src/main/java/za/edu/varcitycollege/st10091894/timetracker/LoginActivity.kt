31fffe9fba0f18c7f7aeb9802f73f90fcc498f8c
Molefe <ST10092063>
2023-06-06 01:13:17 +0200

    Fixed some errors on the login page and implemented some validations for the login page

--- app/src/main/java/za/edu/varcitycollege/st10091894/timetracker/LoginActivity.kt
+++ app/src/main/java/za/edu/varcitycollege/st10091894/timetracker/LoginActivity.kt
@@ -1,29 +1,40 @@
 package za.edu.varcitycollege.st10091894.timetracker
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var etvTaskName: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var loginButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        // for testing purposes
-        val nextPage = findViewById<Button>(R.id.login)
+        etvTaskName = findViewById(R.id.etvTaskName)
+        edtPassword = findViewById(R.id.edtPassword)
+        loginButton = findViewById(R.id.login)
 
-        nextPage.setOnClickListener() {
-            val nextPage = Intent(this, MainActivity::class.java)
-            startActivity(nextPage)
-            finish()
-        }
+        loginButton.setOnClickListener {
+            val username = etvTaskName.text.toString()
+            val password = edtPassword.text.toString()
 
-        val registerPage = findViewById<TextView>(R.id.goToRegister)
-        registerPage.setOnClickListener(){
-            val registerPage = Intent(this, RegisterActivity::class.java)
-            startActivity(registerPage)
+            if (isValidCredentials(username, password)) {
+                // Valid credentials, perform login logic here
+            } else {
+                // Invalid credentials, show an error message
+                etvTaskName.error = "Invalid username or password"
+                edtPassword.error = "Invalid username or password"
+            }
         }
+    }
+
+    private fun isValidCredentials(username: String, password: String): Boolean {
+        // Implement your own validation logic here
+        return username.isNotEmpty() && password.isNotEmpty()
     }
 }
