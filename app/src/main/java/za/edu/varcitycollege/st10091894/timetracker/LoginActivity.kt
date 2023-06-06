832b4b9917bf74cb6680702d1beb77050b20e7f9
Thabiso <st10091894@vcconnect.edu.za>
2023-06-06 09:15:33 +0000

    made changes to the register activity

--- app/src/main/java/za/edu/varcitycollege/st10091894/timetracker/LoginActivity.kt
+++ app/src/main/java/za/edu/varcitycollege/st10091894/timetracker/LoginActivity.kt
@@ -1,8 +1,10 @@
 package za.edu.varcitycollege.st10091894.timetracker
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
@@ -19,6 +21,12 @@ class LoginActivity : AppCompatActivity() {
         edtPassword = findViewById(R.id.edtPassword)
         loginButton = findViewById(R.id.login)
 
+        val registerPage = findViewById<TextView>(R.id.goToRegister)
+        registerPage.setOnClickListener(){
+            val registerPage = Intent(this, RegisterActivity::class.java)
+            startActivity(registerPage)
+            finish()
+        }
         loginButton.setOnClickListener {
             val username = etvTaskName.text.toString()
             val password = edtPassword.text.toString()
